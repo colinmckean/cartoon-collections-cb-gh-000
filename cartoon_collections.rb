4796314255780911fc9dcb052ae4eb85f506a6ae
Colin McKean <ezikill@hotmail.com>
2017-09-08 17:56:11 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(planeteers)
 end
 
 def find_the_cheese(cheese)
-  cheese_types.find{|thingy| thingy == "cheddar" }
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.find{|thingy| thingy == "cheddar" }
 end
